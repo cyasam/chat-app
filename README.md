3b5326b0bf38a2887def952a5be32154d3eed7dd
cyasam <cyasam@gmail.com>
2018-10-28 05:46:47 +0300

    Demo page changed.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Chat App Interface with Authentication (React, Redux)
 
-Demo: http://chat-node.surge.sh/
+Demo: https://chat-app-node-server.herokuapp.com/
 
 Test User
 Email: chatappnode@gmail.com
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
   TOKEN_KEY_NAME: 'jwtToken',
-  API_URL: process.env.NODE_ENV === 'production' ? 'http://18.184.62.255:4567' : 'http://localhost:4567'
+  API_URL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4567'
 };
